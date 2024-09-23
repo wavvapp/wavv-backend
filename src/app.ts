21692a79327c793a943353bf2b4ba9a760067c39
serapieTuyishime <tuyiserapie1@gmail.com>
2024-09-23 18:57:37 +0200

    feat: initialize basic swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import "reflect-metadata";
 import express from "express";
-import { useExpressServer } from "routing-controllers";
+import { getMetadataArgsStorage, useExpressServer } from "routing-controllers";
 import { HomeController } from "./controllers/HomeController";
-
+import { routingControllersToSpec } from "routing-controllers-openapi";
+const swaggerUi = require('swagger-ui-express');
 const app = express();
 
 useExpressServer(app, {
   controllers: [HomeController],
 });
+
+const storage = getMetadataArgsStorage()
+const spec = routingControllersToSpec(storage)
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(spec));
 
 export default app;
--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from "routing-controllers";
+import { Controller, Get, Param, Post, Put } from "routing-controllers";
+import { OpenAPI, routingControllersToSpec } from 'routing-controllers-openapi'
+import { homeControllerDocs } from "../documentation/Home";
 
 @Controller()
 export class HomeController {
   @Get("/api")
+  @OpenAPI(homeControllerDocs.getHello)
   getHello() {
     return "Hello World!";
+  }
+  @Post("/new-user")
+  @OpenAPI(homeControllerDocs.createUser as any)
+  createUser(@Param('userId') userId: string) {
+    return "Please provide the necessary user details";
   }
 }
