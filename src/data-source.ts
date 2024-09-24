cb4685b045fbff37505529c7feb04ffbfe3de69e
sezeranoJchrisostome <sezeranochrisostom123@gmail.com>
2024-09-24 10:22:21 +0200

    chore: initialize typeorm and postgres database

---
+++ src/data-source.ts
@@ -0,0 +1,24 @@
+import { DataSource, DataSourceOptions } from "typeorm";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+
+export const MIGRATION_FILES = ["./src/database/migrations/**/*.ts"];
+const isTest = process.env.NODE_ENV === "test";
+const database = (isTest ? "test_" : "") + process.env.POSTGRES_DB;
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database,
+  migrationsTransactionMode: "each",
+  namingStrategy: new SnakeNamingStrategy(),
+  entities: [],
+  migrationsRun: false,
+  migrations: isTest ? MIGRATION_FILES : [],
+  synchronize: false,
+  poolSize: 10,
+};
+
+export default new DataSource(dataSourceOptions);
