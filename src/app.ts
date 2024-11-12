72885282e9658bcff698eda9409322cc9f4cf6dd
sezeranoJchrisostome <sezeranochrisostom123@gmail.com>
2024-11-12 09:45:02 +0200

    feat: Add item notfound handler

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,10 @@ import {
   useExpressServer,
 } from "routing-controllers";
 import { routingControllersToSpec } from "routing-controllers-openapi";
+import swaggerUi from "swagger-ui-express";
 import { AuthController } from "./controllers/AuthController";
-import { FriendSignalController } from "./controllers/FriendSignalController";
 import { FriendshipController } from "./controllers/FriendshipController";
+import { FriendSignalController } from "./controllers/FriendSignalController";
 import { HomeController } from "./controllers/HomeController";
 import { PointsController } from "./controllers/PointsController";
 import { SignalController } from "./controllers/SignalsController";
@@ -18,15 +19,15 @@ import {
   authorizationChecker,
   currentUserChecker,
 } from "./middlewares/authorization";
+import EntityNotFoundHandler from "./middlewares/EntityNotFoundHandler";
 import { ErrorHandler } from "./middlewares/errorHandler";
 
-const swaggerUi = require("swagger-ui-express");
 const app = express();
 
 const options: RoutingControllersOptions = {
   currentUserChecker,
   authorizationChecker,
-  middlewares: [ErrorHandler],
+  middlewares: [EntityNotFoundHandler, ErrorHandler],
   controllers: [
     HomeController,
     AuthController,
